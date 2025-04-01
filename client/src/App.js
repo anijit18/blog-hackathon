916608e1c953a4ea5bf15483c0d906957db8ab82
anijit18 <anijitpandey2002@gmail.com>
2025-04-02 02:18:19 +0530

    added navbar with functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/Home';
 import LoginPage from './pages/Login';
 import RegisterPage from './pages/Register';
+import Layout from './pages/Layout';
+import AboutPage from './pages/About';
 
 function App() {
   return (
    
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/' element={<Layout />} >
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/about' element={<AboutPage />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ client/src/pages/Layout.js
@@ -0,0 +1,19 @@
+import { Link,Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <>
+        <nav>
+        <ul className="unorderedlist">
+            <li className="list"><Link to="/">Blog Webiste</Link></li>
+            <li className="list"><Link to="/about">About</Link></li>
+            <li className="list"><Link to="/login">Login</Link></li>
+            <li className="list"><Link to="/register">Register</Link></li>
+        </ul>
+        </nav>
+
+        <Outlet />
+        </>
+        
+    )
+}
