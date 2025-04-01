46d94e96bfe0505677aa127faf6bc4ba85c6513e
anijit18 <anijitpandey2002@gmail.com>
2025-04-01 12:30:06 +0530

    post route added

--- server/server.js
+++ server/server.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.post('/', () => {
+  res.send('working');
+})
+
 mongoose.connect(process.env.MONGO_URI,
      {
     useNewUrlParser: true,
