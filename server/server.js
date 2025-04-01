8a3ae9e186e8f60e1d16b0b53ab3a3f5c0d21626
anijit18 <anijitpandey2002@gmail.com>
2025-04-01 12:08:26 +0530

    removed extra code

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv  = require('dotenv');
-const mongoose = require('mongoose');
-
 
 dotenv.config();
 
